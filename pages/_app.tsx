11b15d3539bdb39e8abd3625db2031f19b61a2c7
sebacampos <seba.campos.012@gmail.com>
2020-10-13 10:33:32 -0300

    rm font load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -67,10 +67,6 @@ export default class MyApp extends App {
             rel="icon"
             href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏄‍♂️</text></svg>"
           />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Noto+Serif:wght@700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <NextSeo {...SEO} />
         <Component {...pageProps} />
